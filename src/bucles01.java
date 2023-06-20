e79856984654d4ad91cdcf34a9e5d8bc0a52843f
Elmisio17 <davidalejandrofigueredo@gmail.com>
2023-06-20 11:17:34 -0300

    Ejercicios de Intro progromación tema 4

---
+++ src/bucles01.java
@@ -0,0 +1,10 @@
+public class bucles01 {
+    public static void main(String[] args) {
+        int numeroWhile = -6;
+
+        while (numeroWhile < 3){
+            System.out.println("contador = " + numeroWhile);
+            numeroWhile = numeroWhile + 1;
+        }
+    }
+}
---
+++ src/positivosnegativos.java
@@ -0,0 +1,13 @@
+public class positivosnegativos {
+    public static void main(String[] args) {
+        int numeroIf = -6;
+
+        if (numeroIf > 0){
+            System.out.println(numeroIf + " " + "Es número positivo");
+        }
+        else{
+            System.out.println(numeroIf + " " + "Es número negativo" );
+        }
+
+    }
+}
